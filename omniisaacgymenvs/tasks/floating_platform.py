c2f70a7e78632550fe02007cee78e323088bbc8b
matteo <matteohariry@gmail.com>
2023-02-01 18:03:25 +0100

    - fixed .vs omni. isaac dependencies

--- omniisaacgymenvs/tasks/floating_platform.py
+++ omniisaacgymenvs/tasks/floating_platform.py
@@ -262,11 +262,11 @@ class FloatingPlatformTask(RLTask):
         # resets due to misbehavior
         ones = torch.ones_like(self.reset_buf)
         die = torch.zeros_like(self.reset_buf)
-        die = torch.where(self.target_dist > 5.0, ones, die)
+        die = torch.where(self.target_dist > 5.0, ones, die) # die if going to far from target
 
         # z >= 0.5 & z <= 5.0 & up > 0
-        die = torch.where(self.root_positions[..., 2] < 0.5, ones, die)
-        die = torch.where(self.root_positions[..., 2] > 5.0, ones, die)
+        # die = torch.where(self.root_positions[..., 2] < 0.5, ones, die)
+        # die = torch.where(self.root_positions[..., 2] > 5.0, ones, die)
         # die = torch.where(self.orient_z < 0.0, ones, die)
 
         # resets due to episode length
