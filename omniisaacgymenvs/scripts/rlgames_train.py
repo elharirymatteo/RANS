7a9ca09d267452be73210c35c5c9c8483f73344b
matteo <matteohariry@gmail.com>
2023-03-18 20:04:09 +0100

    -updates

--- omniisaacgymenvs/scripts/rlgames_train.py
+++ omniisaacgymenvs/scripts/rlgames_train.py
@@ -48,7 +48,7 @@ class RLGTrainer():
     def __init__(self, cfg, cfg_dict):
         self.cfg = cfg
         self.cfg_dict = cfg_dict
-
+        #self.cfg.train.params.config.name = "128x128/"+self.cfg.train.params.config.name 
     def launch_rlg_hydra(self, env):
         # `create_rlgpu_env` is environment construction function which is passed to RL Games and called internally.
         # We use the helper function here to specify the environment config.
@@ -72,7 +72,7 @@ class RLGTrainer():
 
         print(f' Experiment name: {self.cfg.train.params.config.name}')
         # dump config dict
-        experiment_dir = os.path.join('runs/fp_masses', self.cfg.train.params.config.name)
+        experiment_dir = os.path.join('runs',self.cfg.wandb_project, self.cfg.train.params.config.name)
         os.makedirs(experiment_dir, exist_ok=True)
         with open(os.path.join(experiment_dir, 'config.yaml'), 'w') as f:
             f.write(OmegaConf.to_yaml(self.cfg))
