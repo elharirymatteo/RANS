97cd2d4167230eb43233444e9d10b26cb0f17889
matteo <matteohariry@gmail.com>
2023-05-22 17:04:25 +0200

    - added velocity derivation from pose message

--- omniisaacgymenvs/scripts/ros_player.py
+++ omniisaacgymenvs/scripts/ros_player.py
@@ -18,54 +18,17 @@ import torch
 import yaml
 #from omni.isaac.core.utils.torch.rotations import *
 import time
-
+from tasks.utils.fp_utils import quaternion_to_rotation_matrix
 from pdb import set_trace as bp
 import os
+from collections import deque
 
 #from rclpy.node import Node
 #from my_msgs.msg import Observation # replace with your observation message type
 #from my_msgs.msg import Action # replace with your action message type
-def quaternion_rotation_matrix(Q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
- 
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
- 
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
-     
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-     
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-     
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-     
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-                            
-    return rot_matrix
-
-def get_observation_from_realsense(msg):
+    
+    
+def get_observation_from_realsense(msg, lin_vel, ang_vel):
     """
     Convert a ROS message to an observation.
     """
@@ -88,10 +51,10 @@ def get_observation_from_realsense(msg):
     # rot_x =  quat_axis(q, 0) #np.random.rand(3)
     # rot_y =  quat_axis(q, 1)
     # rot_z =  quat_axis(q, 2)
-    rot_mat = quaternion_rotation_matrix(q)
+    rot_mat = quaternion_to_rotation_matrix(q)
     lin_vel = [0., 0., 0.]
     ang_vel = [0., 0., 0.]
-    # Cast quaternion to Yaw
+    # Cast quaternion to Yaw    
     siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2])
     cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3])
     # orient_z = torch.arctan2(siny_cosp, cosy_cosp)
@@ -100,8 +63,11 @@ def get_observation_from_realsense(msg):
                         rot_mat[0][0], rot_mat[0][1], rot_mat[0][2], 
                         rot_mat[1][0], rot_mat[1][1], rot_mat[1][2], 
                         rot_mat[2][0], rot_mat[2][1], rot_mat[2][2], 
-                        0, 0, 0, 0, 0, 0, cosy_cosp, siny_cosp]), dtype=torch.float32, device='cuda')
+                        lin_vel[0],lin_vel[1],lin_vel[2],
+                        ang_vel[0],ang_vel[1],ang_vel[2], 
+                        cosy_cosp, siny_cosp]), dtype=torch.float32, device='cuda')
     return obs
+
 
 def enable_ros_extension(env_var: str = "ROS_DISTRO"):
     """
@@ -142,46 +108,78 @@ class MyNode:
         import rospy
         from std_msgs.msg import ByteMultiArray
         from geometry_msgs.msg import PoseStamped
-        import tf2_ros
         self.rospy = rospy
-        self.buffer = []
-        self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        # Initialize variables
+        self.buffer_size = 5  # Number of samples for differentiation
+        self.pose_buffer = deque(maxlen=self.buffer_size)
+        self.time_buffer = deque(maxlen=self.buffer_size)
+        self.act_every = 0 # act every 5 steps
+
+        # Initialize Subscriber and Publisher
         self.sub = rospy.Subscriber("/vrpn_client_node/FPA/pose", PoseStamped, self.callback)
         self.pub = rospy.Publisher("/spacer_floating_platform_a/valves/input", ByteMultiArray, queue_size=10)
         self.player = player
         self.my_msg = ByteMultiArray()
         self.count = 0
-        #bp()
+        self.end_experiment_at_step = 20
+        self.rate = rospy.Rate(5) # 1hz
+
         print("Node initialized")
 
     def callback(self, msg):
 
-        #print(msg)
-        #bp()
-        print(f'count: {self.count}')
+        current_time = self.rospy.Time.now()
 
-        obs = get_observation_from_realsense(msg)
-        #print(obs)
-        #trans = self.tfBuffer.lookup_transform(turtle_name, 'turtle1', rospy.Time())
-        #obs = torch.rand(1, 20, device='cuda')
-        #obs = torch.tensor(msg.data)
-        action = self.player.get_action(obs, is_deterministic=True)
-        action = action.cpu().tolist()        
-        # add lifting action
-        lifting_active = 1
-        action.insert(0, lifting_active)
-        self.my_msg.data = action
-        #print(obs, action)
-        self.pub.publish(self.my_msg)
-        time.sleep(1)
-        self.count += 1
-        if self.count == 20:
+        # Add current pose and time to the buffer
+        self.pose_buffer.append(msg)
+        self.time_buffer.append(current_time)
+        self.act_every += 1
+        # Calculate velocities if buffer is filled
+        if len(self.pose_buffer) == self.buffer_size and self.act_every == self.buffer_size:
+            lin_vel, ang_vel = self.derive_velocities()
+            self.act_every = 0
+
+            obs = get_observation_from_realsense(msg, lin_vel, ang_vel)
+            #obs = torch.rand(1, 20, device='cuda')
+        
+            action = self.player.get_action(obs, is_deterministic=True)
+            action = action.cpu().tolist()        
+            # add lifting action
+            lifting_active = 1
+            action.insert(0, lifting_active)
+            self.my_msg.data = action
+
+            print(f'count: {self.count}')
+            print(obs, action)
+            self.pub.publish(self.my_msg)
+            self.count += 1
+
+        self.rate.sleep()
+
+        if self.count == self.end_experiment_at_step:
             self.my_msg.data = [0,0,0,0,0,0,0,0,0]
             self.pub.publish(self.my_msg)
 
             self.rospy.signal_shutdown("Done")
             print("Shutting down node")
+                
+    def derive_velocities(self):
+        dt = (self.time_buffer[-1] - self.time_buffer[0]).to_sec() # Time difference between first and last pose
+
+        # Calculate linear velocities
+        linear_positions = np.array([[pose.pose.position.x, pose.pose.position.y, pose.pose.position.z] for pose in self.pose_buffer])
+        linear_velocities = np.diff(linear_positions, axis=0) / dt
+        average_linear_velocity = np.mean(linear_velocities, axis=0)
+
+        # Calculate angular velocities
+        angular_orientations = np.array([[pose.pose.orientation.w, pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z] for pose in self.pose_buffer])
+        angular_rot_matrices = np.array([quaternion_to_rotation_matrix(orientation) for orientation in angular_orientations])
+        dR_matrices = np.diff(angular_rot_matrices, axis=0) / dt
+        angular_velocities = np.array([(dR[2, 1], dR[0, 2], dR[1, 0]) for dR in dR_matrices])
+        average_angular_velocity = np.mean(angular_velocities, axis=0)
+
+        return average_linear_velocity, average_angular_velocity
+
 
 @hydra.main(config_name="config", config_path="../cfg")
 def parse_hydra_configs(cfg: DictConfig):
@@ -212,8 +210,6 @@ def parse_hydra_configs(cfg: DictConfig):
     # _____Create ROS node_____
     rospy.init_node('my_node')
     node = MyNode(player)
-    rate = rospy.Rate(1.0)
-    count = 0
     
     rospy.spin()
 
