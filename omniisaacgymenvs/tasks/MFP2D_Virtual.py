ae63d3b512230af12f1ba0d553560cd27ab35b61
AntoineRichard <antoine.richard@gatech.edu>
2023-06-05 18:32:58 +0200

    fixed typos where the wrong config wpuld be used to load floor and noise parameters

--- omniisaacgymenvs/tasks/MFP2D_Virtual.py
+++ omniisaacgymenvs/tasks/MFP2D_Virtual.py
@@ -54,20 +54,20 @@ class MFP2DVirtual(RLTask):
         self.max_floor_force = math.sqrt(self.max_floor_force**2 / 2)
 
         # Add noisy observations
-        self.add_noise_on_pos = self._task_cfg['env']['add_noise_on_pos']
-        self.position_noise_min = self._task_cfg['env']['position_noise_min']
-        self.position_noise_max = self._task_cfg['env']['position_noise_max']
-        self.add_noise_on_vel = self._task_cfg['env']['add_noise_on_vel']
-        self.velocity_noise_min = self._task_cfg['env']['velocity_noise_min']
-        self.velocity_noise_max = self._task_cfg['env']['velocity_noise_max']
-        self.add_noise_on_heading = self._task_cfg['env']['add_noise_on_heading']
-        self.heading_noise_min = self._task_cfg['env']['heading_noise_min']
-        self.heading_noise_max = self._task_cfg['env']['heading_noise_max']
+        #self.add_noise_on_pos = self._task_cfg['env']['add_noise_on_pos']
+        #self.position_noise_min = self._task_cfg['env']['position_noise_min']
+        #self.position_noise_max = self._task_cfg['env']['position_noise_max']
+        #self.add_noise_on_vel = self._task_cfg['env']['add_noise_on_vel']
+        #self.velocity_noise_min = self._task_cfg['env']['velocity_noise_min']
+        #self.velocity_noise_max = self._task_cfg['env']['velocity_noise_max']
+        #self.add_noise_on_heading = self._task_cfg['env']['add_noise_on_heading']
+        #self.heading_noise_min = self._task_cfg['env']['heading_noise_min']
+        #self.heading_noise_max = self._task_cfg['env']['heading_noise_max']
 
         # Add noisy actions
-        self.add_noise_on_act = self._task_cfg['env']['add_noise_on_act']
-        self.min_action_noise = self._task_cfg['env']['min_action_noise']
-        self.max_action_noise = self._task_cfg['env']['max_action_noise']
+        #self.add_noise_on_act = self._task_cfg['env']['add_noise_on_act']
+        #self.min_action_noise = self._task_cfg['env']['min_action_noise']
+        #self.max_action_noise = self._task_cfg['env']['max_action_noise']
 
         # Platform parameters
         self.dt = self._task_cfg["sim"]["dt"]
