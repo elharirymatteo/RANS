5b66a01d5901b6670e3a57f3862ee8b1c45aac2b
AntoineRichard <antoine.richard@gatech.edu>
2023-06-07 13:08:55 +0200

    Removed penalty from task reward.

--- omniisaacgymenvs/tasks/virtual_floating_platform/MFP2D_go_to_pose.py
+++ omniisaacgymenvs/tasks/virtual_floating_platform/MFP2D_go_to_pose.py
@@ -57,15 +57,6 @@ class GoToPoseTask(Core):
         # rewards
         self.position_reward, self.heading_reward = self._reward_parameters.compute_reward(current_state, actions, self.position_dist, self.heading_dist)
       
-        # adding energy penalty
-        self.position_reward -= self._task_parameters.energy_penalty *  actions.sum(-1)
-
-        # adding velocity penalty
-        self.position_reward -= self._task_parameters.velocity_penalty *  current_state['linear_velocity'].sum(-1)
-
-        #adding angular velocity penalty
-        self.heading_reward -= self._task_parameters.velocity_penalty *  current_state['angular_velocity'].sum(-1)
-
         return self.position_reward + self.heading_reward
     
     def update_kills(self):
