bd0b9d17580109c75a4b72b4840c1384fac11d2a
matteo <matteohariry@gmail.com>
2023-06-12 15:13:44 +0200

    - added plot of all episodes trajectories evaluated

--- omniisaacgymenvs/utils/plot_experiment.py
+++ omniisaacgymenvs/utils/plot_experiment.py
@@ -92,6 +92,26 @@ def plot_episode_data_virtual(ep_data, save_dir, all_agents=False):
         
         plt.savefig(save_dir + '_mean_actions_hist')
 
+        # °°°°°°°°°°°°°°°°°°°°°°°° plot all the episodes trajectories in the 2d plane  °°°°°°°°°°°°°°°°°°°°°°°°°
+      
+        fig_count += 1
+        plt.figure(fig_count)
+        plt.clf()    
+        colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+        positions = state_history[:, :, 6:8]
+        print(positions.shape)
+
+        for j in range(positions.shape[1]):
+            col = colours[j % len(colours)]
+            plt.plot(positions[:, j, 0], positions[:, j, 1], color=col, alpha=0.5, linewidth=0.75)
+            plt.xlabel('X [m]')
+            plt.ylabel('Y [m]')
+
+        plt.title(f'Trajectories in 2D space [{positions.shape[1]} episodes]')
+        plt.gcf().dpi = 200
+        plt.savefig(save_dir + '_multi_traj')
+        # plt.show()
+
         return
 
 
@@ -114,10 +134,20 @@ def plot_one_episode(ep_data, save_dir):
 
     # save data to csv file
     pd.DataFrame.to_csv(pd.DataFrame(control_history), save_dir + 'actions.csv')
+    # setting the right task_data lavels based on the task flag.
+    task_flag = state_history[0, 5].astype(int)
+    if task_flag == 0:
+        task_data_label = 'error_x', 'error_y'
+    elif task_flag == 1:
+        task_data_label = 'error_x', 'error_y', 'cos_error_heading', 'sin_error_heading'
+    elif task_flag == 2:
+        task_data_label = 'error_vx', 'error_vy'
+    elif task_flag == 3:
+        task_data_label = 'error_vx', 'error_vy', 'error_omega'
+
     pd.DataFrame.to_csv(pd.DataFrame(state_history[:, :8], columns=['cos_theta','sin_theta', 
                                                                     'lin_vel_x', 'lin_vel_y', 
-                                                                    'ang_vel_z', 'task_flag',
-                                                                    'error_dist_x', 'error_dist_y']),
+                                                                    'ang_vel_z', 'task_flag'].append(task_data_label)),
                                      save_dir + 'states_episode.csv')
 
     tgrid = np.linspace(0, len(control_history), len(control_history))
