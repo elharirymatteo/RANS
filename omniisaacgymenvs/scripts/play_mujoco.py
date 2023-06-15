eff95e18488db688516da218dfc94f9f0773eb85
matteo <matteohariry@gmail.com>
2023-06-15 11:35:14 +0200

    - fixed small bug in the eval_metrics

--- omniisaacgymenvs/scripts/play_mujoco.py
+++ omniisaacgymenvs/scripts/play_mujoco.py
@@ -19,9 +19,9 @@ from rl_games.algos_torch.players import BasicPpoPlayerContinuous, BasicPpoPlaye
 
 
 
-config_name = "/home/antoine/Documents/Orbitals/Omniverse/omniisaacgymenvs/cfg/train/MFP2D_PPOmulti_dict_MLP.yaml"
+config_name = "/home/matteo/Projects/OmniIsaacGymEnvs/omniisaacgymenvs/cfg/train/MFP2D_PPOmulti_dict_MLP.yaml"
 #model_name = "/home/antoine/Documents/Omniverse/omniisaacgymenvs/runs/MFP2D_Virtual_GoToXY/nn/last_MFP2D_Virtual_GoToXY_ep_1000_rew__607.5902_.pth"
-model_name = "/home/antoine/Documents/Orbitals/Omniverse/penalty/MLP_GTXY_UF_0.25_ST_PE_0.03_PAV_1.5_PLV_0.01/nn/last_MLP_GTXY_UF_0.25_ST_PE_0.03_PAV_1.5_PLV_0.01_ep_2000_rew__555.7034_.pth"
+model_name = "/home/matteo/Projects/OmniIsaacGymEnvs/penalty/MLP_GTXY_UF_0.25_ST_PE_0.03_PAV_1.5_PLV_0.01/nn/last_MLP_GTXY_UF_0.25_ST_PE_0.03_PAV_1.5_PLV_0.01_ep_2000_rew__555.7034_.pth"
 
 class RLGamesModel:
     def __init__(self):
