a06645d4b4326fd83dcf70a7411cddb6ee53bfce
matteo <matteohariry@gmail.com>
2023-06-20 01:24:47 +0200

    -  new experiments (lighter mass 5.32kg)

--- omniisaacgymenvs/utils/plot_experiment.py
+++ omniisaacgymenvs/utils/plot_experiment.py
@@ -81,6 +81,7 @@ def plot_episode_data_virtual(ep_data, save_dir, all_agents=False):
         plt.gca().xaxis.set_minor_locator(minor_locator)
 
         data = np.array([np.sum(control_history[:, i, :], axis=0) for i in range(control_history.shape[1])])
+        print()
         n, bins, patches = plt.hist(np.mean(data, axis=1, dtype=int), bins=n_bins, edgecolor='white')
         xticks = [(bins[idx+1] + value)/2 for idx, value in enumerate(bins[:-1])]
         ticklabels = [f'T{i+1}' for i in range(n_bins)]
