546d183d07123aceb27e230d18d20651930e9698
AntoineRichard <antoine.richard@gatech.edu>
2023-08-09 19:36:10 +0200

    Proper way to launch experiments without an hardcoded path.

--- omniisaacgymenvs/scripts/run_experiments.py
+++ omniisaacgymenvs/scripts/run_experiments.py
@@ -1,17 +1,23 @@
 import json
 import subprocess
+import argparse
 
-# Load the configuration file
-with open('./exp_icra.json', 'r') as f:
-    experiments = json.load(f)
+parser = argparse.ArgumentParser("Processes one or more experiments.")
+parser.add_argument("--exps", type=str, nargs="+", default=None, help="List of path to the experiments' config to be ran.")
+args, unknown_args = parser.parse_known_args()
 
-# Loop through each experiment and execute it
-for experiment_name, arguments in experiments.items():
-    
-    # Construct the command to execute the experiment
-    cmd = ['/home/matteo/.local/share/ov/pkg/isaac_sim-2022.2.1/python.sh', 'scripts/rlgames_train.py']
-    for arg, value in arguments.items():
-        cmd.extend(['{}'.format(arg)+"="+str(value)])
-    print(f'Running command: {" ".join(cmd)}')
-    # Execute the command
-    subprocess.run(cmd)
+for exp in args.exps:
+    # Load the configuration file
+    with open(exp, 'r') as f:
+        experiments = json.load(f)
+
+    # Loop through each experiment and execute it
+    for experiment_name, arguments in experiments.items():
+
+        # Construct the command to execute the experiment
+        cmd = ['/home/matteo/.local/share/ov/pkg/isaac_sim-2022.2.1/python.sh', 'scripts/rlgames_train.py']
+        for arg, value in arguments.items():
+            cmd.extend(['{}'.format(arg)+"="+str(value)])
+        print(f'Running command: {" ".join(cmd)}')
+        # Execute the command
+        subprocess.run(cmd)
