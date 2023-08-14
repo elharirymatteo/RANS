- fixed angular vel performance metric (absolute before average)

omniisaacgymenvs/scripts/multi_model_eval.py
@@ -96,7 +96,7 @@ def eval_multi_agents(agent, models, horizon, load_dir):
         # Collect the data for the success rate table        
         success_rate_df = success_rate_from_distances(ep_data['all_dist'],threshold=0.02)
         success_rate_df['avg_rew'] = [np.mean(ep_data['rews'])]
-        ang_vel_z = ep_data['obs'][:, :, 4:5][:,:,0]
+        ang_vel_z = np.absolute(ep_data['obs'][:, :, 4:5][:,:,0])
         success_rate_df['avg_ang_vel'] = [np.mean(ang_vel_z.mean(axis=1))]
         lin_vel_x = ep_data['obs'][:, 2:3]
         lin_vel_y = ep_data['obs'][:, 3:4]
@@ -109,16 +109,16 @@ def eval_multi_agents(agent, models, horizon, load_dir):
         # If want to print the latex code for the table use the following line
 
     # create index for the dataframe and save it
-    model_names = [model.split("/")[2] for model in models]
+    model_names = [model.split("/")[3] for model in models]
     all_success_rate_df.insert(loc=0, column="model", value=model_names)
-    all_success_rate_df.to_csv(evaluation_dir + "multi_model_performance_2.csv")
+    all_success_rate_df.to_csv(evaluation_dir + "multi_model_performance.csv")
 
 
 @hydra.main(config_name="config", config_path="../cfg")
 def parse_hydra_configs(cfg: DictConfig):
     
     # specify the experiment load directory
-    load_dir = "./icra24_noise/" #+ "expR/"
+    load_dir = "./icra24/" + "expR/"
     experiments = os.listdir(load_dir)
     print(f'Experiments found in {load_dir} folder: {len(experiments)}')
     models = get_valid_models(load_dir, experiments)