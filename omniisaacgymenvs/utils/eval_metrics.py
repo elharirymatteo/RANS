af0c4ea438bdc50ac13bcc10a831182f53a15c3e
matteo <matteohariry@gmail.com>
2023-09-12 18:09:12 +0200

    - added new metrics

--- omniisaacgymenvs/utils/eval_metrics.py
+++ omniisaacgymenvs/utils/eval_metrics.py
@@ -32,6 +32,33 @@ def print_success(success_rate_thr: float, success_rate_thr2: float, success_and
         print(f'Success rate with threshold {threshold/2}: {success_rate_thr2}')
         print(f'Success rate and stay with margin {threshold*7.5}: {success_and_stay_rate * 100}')
 
+
+def get_GoToPose_success_rate_new(ep_data:dict, threshold:float = 0.02, print_intermediate:bool = False) -> dict:
+    """Compute the success rate from the distances to the target.
+
+    Args:
+        distances (np.ndarray): Array of distances to the target for N episodes.
+        precision (float): Distance at which the target is considered reached.
+    Returns:
+        float: Success rate."""
+
+    distances = np.linalg.norm(ep_data['obs'][:, :, 6:8], axis=2)
+    dist = distances
+    avg_p005 = np.mean([dist < 0.05])
+    avg_p002 = np.mean([dist < 0.02])
+    avg_p001 = np.mean([dist < 0.01])
+    heading = np.arctan2(ep_data['obs'][:,:,-1], ep_data['obs'][:,:,-2])
+    avg_h005 = np.mean([heading < np.pi*5/180])
+    avg_h002 = np.mean([heading < np.pi*2/180])
+    avg_h001 = np.mean([heading < np.pi*1/180])
+    print("percentage of time spent under (5cm, 2cm, 1cm):",avg_p005*100, avg_p002*100, avg_p001*100)
+    print("percentage of time spent under (5deg, 2deg, 1deg):",avg_h005*100, avg_h002*100, avg_h001*100)
+    success_rate_df = pd.DataFrame({'PT5': [avg_p005], 'PT2': [avg_p002], 'PT1': [avg_p001],
+                                    'OT5': [avg_h005], 'OT2': [avg_h002], 'OT1': [avg_h001]})
+    
+    return {"position":success_rate_df}
+
+    
 def get_GoToXY_success_rate(ep_data:dict, threshold:float = 0.02, print_intermediate:bool = False) -> dict:
     """Compute the success rate from the distances to the target.
 
