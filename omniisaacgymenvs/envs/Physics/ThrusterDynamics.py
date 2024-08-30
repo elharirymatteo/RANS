Fix ThrusterDynamics bug and adjust rotation drag
ThrusterDynamics bug.
The Thurster Dynamics uses a lookup table to map the input commands
(-1.0,1.0) to forces.  The index for the lookup table calculation was
offset by 1 causing the index -1 to be used instead of the 0. Accessing
the last value of the lookup table instead of the first and providing
the opposite force.

Rotation drag parameter.
Adjusted observing the circle trajectories of the boat in real world
experiments. The new value reduces the gap between simulation and
reality.

omniisaacgymenvs/envs/Physics/ThrusterDynamics.py
@@ -184,13 +184,8 @@ class DynamicsFirstOrder(Dynamics):
         # Debug: print(cmd_value)
         # print(f"cmd_value: {cmd_value}")
         # cmd_value is size (num_envs,2)
-        idx_left = torch.round(((cmd_value[:, 0] + 1) / 2 * self.n_left) - 1).to(
-            torch.long
-        )
-        idx_right = torch.round(((cmd_value[:, 1] + 1) / 2 * self.n_right) - 1).to(
-            torch.long
-        )
-        # print(f"idx_left: {idx_left}")
+        idx_left = torch.round((cmd_value[:, 0] + 1) / 2 * (self.n_left -1)).to(torch.long)
+        idx_right = torch.round((cmd_value[:, 1] + 1) / 2 * (self.n_right -1)).to(torch.long)
         # Using indices to gather interpolated forces for each thruster
         self.thruster_forces_before_dynamics[:, 0] = self.y_linear_interp_left[idx_left]
         self.thruster_forces_before_dynamics[:, 1] = self.y_linear_interp_right[